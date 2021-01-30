874238c99c163b0b9892980a005667b1787b904d
Brenton Bongcaron <brenton3926@gmail.com>
2021-01-30 16:49:43 -0500

    Shows greyscale path to goal

--- render.py
+++ render.py
@@ -1,39 +1,71 @@
 from maze import *
-import pygame
+import pygame, numpy, copy
 size = 360
 
-def grid(window, maze):
-    spaceDim = size // len(maze)
+def grid(window, maze, numMoves=1):
+    dim = len(maze)
+    spaceDim = size // dim
     x = 0
     y = 0
+    if numMoves is not None:
+        grayscale = numpy.linspace(18,238,numMoves,dtype='int')
     for row in maze:
         for space in row:
-            spaceBorder = pygame.Rect(x,y,spaceDim, spaceDim)
+            newSpace = pygame.Rect(x, y, spaceDim, spaceDim)
             if space == 0:
-                newSpace = pygame.Rect(x,y,spaceDim, spaceDim)
                 pygame.draw.rect(window, (0,0,0), newSpace, width=0)
                 pygame.draw.rect(window, (0,0,0), newSpace, width=1)
             elif space == 1:
-                newSpace = pygame.Rect(x,y,spaceDim, spaceDim)
                 pygame.draw.rect(window, (255,255,255), newSpace, width=0)
                 pygame.draw.rect(window, (0,0,0), newSpace, width=1)
             elif space == 2:
-                newSpace = pygame.Rect(x,y,spaceDim, spaceDim)
                 pygame.draw.rect(window, (255,0,0), newSpace, width=0)
+                pygame.draw.rect(window, (0,0,0), newSpace, width=1)
+            elif space == 3:
+                pygame.draw.rect(window, (grayscale[numMoves-1], grayscale[numMoves-1], grayscale[numMoves-1]), newSpace, width=0)
+                pygame.draw.rect(window, (0,0,0), newSpace, width=1)
+                numMoves -= 1
+            elif space == -1:
+                pygame.draw.rect(window, (0,255,255), newSpace, width=0)
                 pygame.draw.rect(window, (0,0,0), newSpace, width=1)
             x += spaceDim
         y += spaceDim
         x = 0
+    goalSpace = pygame.Rect(dim - 1 ,dim - 1, spaceDim, spaceDim)
+    pygame.draw.rect(window, (255,255,0), newSpace, width=0)
+    pygame.draw.rect(window, (0,0,0), newSpace, width=1)
+
+def path(window, maze, prev):
+    returnMaze = copy.deepcopy(maze)
+    dim = len(maze)
+    spaceDim = size // dim
+    # prev is None when a search algorithm has failed to find a path
+    if prev is None:
+        #print("No solution")
+        return maze, None
+    # Loop and count the number of moves in the path found by the search algorithm
+    numMoves = 0
+    currentSpace = (dim - 1, dim - 1)
+    while currentSpace != (0,0):
+        currentSpace = prev[currentSpace]
+        returnMaze[currentSpace[0]][currentSpace[1]] = 3
+        numMoves += 1
+    return returnMaze, numMoves
 
 def redraw(window, maze):
     window.fill((255,255,255))
-    grid(window, maze)
+    prev = DFS(maze)
+    solvedMaze, numMoves = path(window, maze, prev)
+
+    grid(window, solvedMaze, numMoves)
+
     pygame.display.update()
 
 def render():
     maze = buildMaze(10, 0.30, 0.1)
-    for row in maze:
-        print(row)
+    agentLocation = (0,0)
+    maze[0][0] = -1
+
     rows = len(maze)
 
     window = pygame.display.set_mode((size,size))
