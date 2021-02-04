cd86971882b59f72c8eb33f101b92ba80c6f390d
BLIGHT <abevit2000@gmail.com>
2021-02-03 22:01:44 -0500

    Cost changes made. LaTeX Title page slightly made.

--- render.py
+++ render.py
@@ -65,7 +65,7 @@ def movementOne(window, maze, firep, algorithm):
     elif algorithm == "a*":
         prev = aStar(maze, agentLocation)[0]
     elif algorithm == "a*+":
-        prev = aStarPlus(maze, agentLocation)[0]
+        prev = aStarPlus(maze, agentLocation, [], firep)[0]
     else:
         return False
     # The "Game loop"
@@ -132,7 +132,7 @@ def movementTwo(window, maze, firep, algorithm):
         elif algorithm == "a*":
             prev = aStar(maze, agentLocation, spacesTraveled)[0]
         elif algorithm == "a*+":
-            prev = aStarPlus(maze, agentLocation, spacesTraveled)[0]
+            prev = aStarPlus(maze, agentLocation, spacesTraveled, firep)[0]
         else:
             return False
         currentSpace = (dim - 1, dim - 1)
